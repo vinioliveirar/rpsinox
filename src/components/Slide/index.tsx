64f27afa9990d3679de0fc3d0af1019081126771
Vinicius Oliveira Rocha <vinicius@Mac-mini-de-Stefany.local>
2023-02-16 18:03:22 -0300

    feat(json): create and using json server simulator

--- src/components/Slide/index.tsx
+++ src/components/Slide/index.tsx
@@ -1,11 +1,30 @@
 import "swiper/css";
 import "swiper/css/navigation";
-import { Swiper } from "swiper/react";
 import { Autoplay, Navigation } from "swiper";
 import { SwiperContainer, SwiperSlideContent, SwiperSlideStyled, SwiperWrapper } from "./styles";
 import { BudgetButton } from "../BudgetButton";
+import { useEffect, useState } from "react";
+
+interface Informations {
+  id: number;
+  image: string;
+  title: string;
+  description: string;
+  stongDescription: string;
+}
 
 export default function Slide() {
+  const [informations, setInformations] = useState<Informations[]>([]);
+
+  //puxa dados do JSONServer
+  async function loadInformations() {
+    const response = await fetch("http://localhost:3000/informations");
+    const data = await response.json();
+    setInformations(data);
+  }
+  useEffect(() => {
+    loadInformations();
+  }, []);
   return (
     <>
       <SwiperContainer
@@ -17,8 +36,32 @@ export default function Slide() {
         navigation={true}
         modules={[Autoplay, Navigation]}
       >
-        <SwiperSlideStyled>
-          <img src="https://www.rpsinox.com.br/images/slides/mequi-1000-2.jpg" alt="" />
+        {informations.map((information) => {
+          return (
+            <SwiperSlideStyled>
+              <img src={`/images/${information.image}`} alt="" />
+              <SwiperWrapper>
+                <SwiperSlideContent>
+                  <div>
+                    <h2>{information.title}</h2>
+                  </div>
+                  <div>
+                    <p>
+                      {information.description} <br />
+                      <strong>{information.stongDescription}</strong>
+                    </p>
+                  </div>
+                  <div>
+                    <BudgetButton text="Ver Projetos" />
+                  </div>
+                </SwiperSlideContent>
+              </SwiperWrapper>
+            </SwiperSlideStyled>
+          );
+        })}
+
+        {/* <SwiperSlideStyled>
+          <img src={`/images/${informations.image}`} alt="" />
           <SwiperWrapper>
             <SwiperSlideContent>
               <div>
@@ -35,7 +78,7 @@ export default function Slide() {
               </div>
             </SwiperSlideContent>
           </SwiperWrapper>
-        </SwiperSlideStyled>
+        </SwiperSlideStyled> */}
 
         {/* <SwiperSlideStyled>
           <img src="https://www.rpsinox.com.br/images/slides/hospital-corrimao.jpg" alt="" />
